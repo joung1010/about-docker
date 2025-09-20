2159caadb41b24a88b06446a8b91fe59cfb3684b
mason park <joung4342@gmail.com>
2025-09-20 22:09:32 +0900

    docs(docker): 도커컴포즈 실습3

--- src/main/java/com/app/docker/controller/BaseController.java
+++ src/main/java/com/app/docker/controller/BaseController.java
@@ -1,17 +1,24 @@
 package com.app.docker.controller;
 
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RequiredArgsConstructor
 
 @RestController
 public class BaseController {
 
+    private final  RedisTemplate<String, Object> redisTemplate;
+
     @GetMapping("")
     public String index() {
+        redisTemplate.opsForValue().set("abc", "def");
+
         return "Hello World";
     }
 
